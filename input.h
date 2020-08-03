5f0b1c1aedbe028728cd7d7f1574e8df296db6c3
TwoDog-W <69105036+TwoDog-W@users.noreply.github.com>
2020-08-03 09:16:08 +0800

    Delete input.h

--- input.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INPUT.H__
-#define _INPUT.H__
-typedef union{
-        unsigned int addr_all;
-        unsigned char addr_one [sizeof(int)];
-}ip_addr;
-
-void input(ip_addr * p,char * str);
-#endif
